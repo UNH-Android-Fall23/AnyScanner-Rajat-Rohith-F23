11e644511fed3ce3186b24c56648baf99c1f73ff
sihtodstel <rajat.olhan@outlook.com>
2023-10-03 16:19:01 -0400

    Gitignore updated and main activity created

---
+++ app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.unh.anyscanner_rajat_rohith_f23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
