902c129e0ab50d7a13bca56cb5fb1e0b127f0b28
sihtodstel <rajat.olhan@outlook.com>
2023-10-03 16:08:03 -0400

    First commit

---
+++ app/src/androidTest/java/com/unh/anyscanner_rajat_rohith_f23/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.unh.anyscanner_rajat_rohith_f23
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.unh.anyscanner_rajat_rohith_f23", appContext.packageName)
+    }
+}
