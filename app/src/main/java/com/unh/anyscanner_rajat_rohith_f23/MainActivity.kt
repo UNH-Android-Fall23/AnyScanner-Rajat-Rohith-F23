279a24ffbf02c6b5c149354b8df0d0427f3d5eb5
sihtodstel <rajat.olhan@outlook.com>
2023-10-05 23:16:16 -0400

    Registration Activity created, firebase integration tested... New Activity created for tabs

--- app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/MainActivity.kt
+++ app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.unh.anyscanner_rajat_rohith_f23
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,7 +13,7 @@ import com.unh.anyscanner_rajat_rohith_f23.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fbaseAuth: FirebaseAuth
-    private val TAG = "Login_Signup"
+    private val TAG = "LoginActivity"
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +29,26 @@ class MainActivity : AppCompatActivity() {
             val password=binding.passwordEt
 
             accountLogin(username.text.toString(),password.text.toString())
+            val intent = Intent(this, AnyScannerActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.registerText.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.fingerprintText.setOnClickListener {
+
+            FirebaseAuth.getInstance().signOut();
+            val currentUser=fbaseAuth.currentUser
+            if(currentUser!=null){
+                Log.d(TAG,"Logout Unsuccesful")
+            }else{
+                Log.d(TAG,"Logout")
+            }
+
         }
     }
 
