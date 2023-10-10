8877506a93d20e520e0f7c5f71b499998cb53d5b
Rohith <tangadipallirohith@gmail.com>
2023-10-09 20:10:36 -0400

    Login user test cases completed

--- app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/MainActivity.kt
+++ app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.unh.anyscanner_rajat_rohith_f23
 
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthMultiFactorException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.unh.anyscanner_rajat_rohith_f23.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "IcebreakerAndroidF23Tag"
     private lateinit var fbaseAuth: FirebaseAuth
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +33,39 @@ class MainActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener {
             val username = binding.editTextTextEmailAddress2
             val password = binding.editTextTextPassword
-            fbaseAuth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                        val user = fbaseAuth.currentUser
-                    if(user!=null){
-                        goToqrActivity(view = null)
-                    }
-                    }
-                }
+            if (username.text.toString() == "") {
+                binding.editTextTextEmailAddress2.error = "Field Required"
+            } else if (password.text.toString() == "") {
+                binding.editTextTextPassword.error = "Field Required"
+            } else {
+                    fbaseAuth.signInWithEmailAndPassword(
+                        username.text.toString(),
+                        password.text.toString()
+                    )
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                val user = fbaseAuth.currentUser
+                                if (user != null) {
+                                    goToqrActivity(view = null)
+                                }
+                            } else {
+                                when ((task.exception as FirebaseAuthException?)!!.errorCode) {
+                                    "ERROR_INVALID_EMAIL" -> binding.editTextTextEmailAddress2.error =
+                                        "INVALID EMAIL"
+
+                                    "ERROR_WRONG_PASSWORD" -> binding.editTextTextPassword.error = "INVALID PASSWORD"
+
+                                    else -> {
+                                        binding.editTextTextEmailAddress2.error = "INVALID EMAIL"
+                                        binding.editTextTextPassword.error = "INVALID PASSWORD"
+                                    }
+                                }
+                            }
+                        }
+            }
         }
+    }
+
     fun goToqrActivity(view: View?) {
         val intent = Intent(this, Qr_Activity::class.java)
         startActivity(intent)
@@ -50,7 +78,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val currentUser = fbaseAuth.currentUser
         if (currentUser != null) {
-            goToqrActivity(view = null)
+           goToqrActivity(view = null)
         }
     }
 }
