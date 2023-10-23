d7eedde6fd157c232582e09c6e1b948e16a92a62
Rohith <tangadipallirohith@gmail.com>
2023-10-23 19:25:40 -0400

    saving biometric,notifications,darkmode preferences for that user account

--- app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/ProfileFragment.kt
+++ app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.unh.anyscanner_rajat_rohith_f23.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -22,6 +23,53 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val user = FirebaseAuth.getInstance().currentUser
+        val db = FirebaseFirestore.getInstance()
+        var b=false
+        var d=false
+        var n=false
+
+        db.collection("UserProfile")
+            .document(user?.uid ?: "")
+            .get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    val userProfile = document.toObject(UserProfile::class.java)
+                    if (userProfile != null) {
+                        binding.switch5.isChecked = userProfile.biometric
+                        binding.switch4.isChecked = userProfile.darkMode
+                        binding.switch3.isChecked = userProfile.notifications
+                    }
+                }
+            }
+
+        binding.switch5.setOnCheckedChangeListener { buttonView, isChecked ->
+        b=isChecked
+            val userProfile = UserProfile(b,d,n)
+
+            db.collection("UserProfile")
+                .document(user?.uid ?: "")
+                .set(userProfile)
+        }
+
+        binding.switch4.setOnCheckedChangeListener { buttonView, isChecked ->
+            d=isChecked
+            val userProfile = UserProfile(b,d,n)
+
+            db.collection("UserProfile")
+                .document(user?.uid ?: "")
+                .set(userProfile)
+        }
+
+        binding.switch3.setOnCheckedChangeListener { buttonView, isChecked ->
+            n=isChecked
+            val userProfile = UserProfile(b,d,n)
+
+            db.collection("UserProfile")
+                .document(user?.uid ?: "")
+                .set(userProfile)
+        }
+
         binding.button8.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             goToLoginActivity(view = null)
@@ -32,6 +80,7 @@ class ProfileFragment : Fragment() {
         binding.textView12.setOnClickListener {
             goToAIActivity(view = null)
         }
+
     }
 
     fun goToLoginActivity(view: View?) {
@@ -43,3 +92,9 @@ class ProfileFragment : Fragment() {
         startActivity(intent)
     }
 }
+
+data class UserProfile(
+    val biometric: Boolean = false,
+    val darkMode: Boolean = false,
+    val notifications: Boolean = false
+)
