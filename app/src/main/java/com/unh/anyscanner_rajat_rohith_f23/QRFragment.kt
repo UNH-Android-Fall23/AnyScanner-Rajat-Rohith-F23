7f1152dc65998e885175a5c8827be5456de162c0
sihtodstel <rajat.olhan@outlook.com>
2023-10-25 21:20:21 -0400

    Buttons added and function verified with VirusTotal API

--- app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/QRFragment.kt
+++ app/src/main/java/com/unh/anyscanner_rajat_rohith_f23/QRFragment.kt
@@ -1,7 +1,12 @@
 package com.unh.anyscanner_rajat_rohith_f23
 
 import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -21,10 +26,12 @@ import org.json.JSONObject
 
 class QRFragment : Fragment() {
     private lateinit var codeScanner: CodeScanner
-    private var scanVal: String = ""
+    var scanVal: String = ""
+    var urlScanned: String=" "
     private val TAG="AnyScannerF23"
     private val apiKey = "408ac8b1b8565046b21e57b71c2ddb72e10bf0e23745cc21fc8cc68a5ef65292"
     private val client = OkHttpClient()
+    private val handler = Handler(Looper.getMainLooper())
 
 
 
@@ -34,11 +41,26 @@ class QRFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_q_r, container, false)
     }
 
-    private fun showQRResults(message: String){
+    private fun showQRResults(title:String, message: String){
         val alertDialog: AlertDialog.Builder=AlertDialog.Builder(context)
         //TODO design custom layout
-        alertDialog.setTitle(scanVal)
+        alertDialog.setTitle(title)
         alertDialog.setMessage(message)
+        alertDialog.setNegativeButton("Close") { dialog, _ ->
+            dialog.dismiss() // Close the dialog
+        }
+        alertDialog.setPositiveButton("Visit") { _, _ ->
+            // Create an Intent to open the URL in a web browser
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(message))
+
+            // Check if there's an app to handle the intent before starting the activity
+            if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                requireActivity().startActivity(intent)
+            } else {
+                // Handle the case where there's no app to handle the intent
+                Toast.makeText(context, "No app to handle this URL.", Toast.LENGTH_SHORT).show()
+            }
+        }
         val dialog: AlertDialog = alertDialog.create()
         dialog.show()
     }
@@ -57,6 +79,7 @@ class QRFragment : Fragment() {
             }
 
             override fun onResponse(call: Call, response: Response) {
+
                 if (response.isSuccessful) {
                     val responseBody = response.body?.string()
                     // Parse and process the URL analysis report as needed
@@ -67,18 +90,34 @@ class QRFragment : Fragment() {
                         .getJSONObject("total_votes")
                     val harmlessCount = totalVotes.getInt("harmless")
                     val maliciousCount = totalVotes.getInt("malicious")
-                    if(harmlessCount>maliciousCount){
+                    val lastVotes=data.getJSONObject("data")
+                        .getJSONObject("attributes")
+                        .getJSONObject("last_analysis_stats")
+                    val lastharmlessCount = lastVotes.getInt("harmless")
+                    val lastmaliciousCount = lastVotes.getInt("malicious")
+                    urlScanned=data.getJSONObject("data")
+                        .getJSONObject("attributes").getString("url")
+
+                    if(harmlessCount>maliciousCount || lastharmlessCount>lastmaliciousCount){
                         scanVal="Harmless"
-                    }else if(maliciousCount>harmlessCount){
+                    }else if(maliciousCount>harmlessCount || lastmaliciousCount>lastharmlessCount){
                         scanVal="Malicious"
                     }
+
                     Log.d(TAG, "Harmless and mal counts are $harmlessCount and $maliciousCount")
+                    Log.d(TAG,"Scan and url $scanVal and $urlScanned")
                 } else {
                     Log.e(TAG, "Request failed with code: ${response.code}")
                     scanVal="Error"
+                   urlScanned="error"
                 }
+
             }
         })
+        Log.d(TAG,"Scan and url outer $scanVal and $urlScanned")
+        handler.postDelayed({
+            showQRResults(scanVal,urlScanned)
+        },3000)
 
         /**/
     }
@@ -94,7 +133,6 @@ class QRFragment : Fragment() {
                     StandardCharsets.UTF_8))
                 checkURL(urlId)
                 Toast.makeText(activity, url, Toast.LENGTH_LONG).show()
-                showQRResults(url)
             }
         }
         scannerView.setOnClickListener {
